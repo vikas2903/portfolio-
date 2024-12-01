9011034e4e417ebaa62d0f34b376446932e642cd
Vikas Prasad <76564198+vikas2903@users.noreply.github.com>
2024-12-01 15:39:00 +0530

    pushed updated code

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// const menuBtn = document.querySelector(".toggle-icon");
+// const toggleBtnClose = document.querySelector(".close");
+// const openMenu = document.querySelector(".menu");
+
+// menuBtn.addEventListener("click", function () {
+//     openMenu.classList.add("active");
+// });
+
+// toggleBtnClose.addEventListener("click", function () {
+//     openMenu.classList.remove("active");
+// });
